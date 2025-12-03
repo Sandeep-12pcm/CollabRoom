edc83ad31e6054fb0d2b230dc06b977f8bba159b
Sandeep <sandeep12pcm@gmail.com>
2025-12-03 17:22:10 +0530

    rollback to previous index.html

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
+import "./wdyr";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
+
 
 createRoot(document.getElementById("root")!).render(
   <>
---
+++ client/src/wdyr.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+if (import.meta.env.DEV) {
+  const { default: whyDidYouRender } = await import(
+    "@welldone-software/why-did-you-render"
+  );
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
