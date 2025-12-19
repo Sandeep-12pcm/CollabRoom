75f677674895301a38b39428af6207a5b18697eb
Sandeep <sandeep12pcm@gmail.com>
2025-12-19 12:54:15 +0530

    added google adsense for all users

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,5 @@
 import "./wdyr";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,9 +8,9 @@ import { Analytics } from "@vercel/analytics/react"
 
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <React.StrictMode>
     <App />
     <SpeedInsights />
     <Analytics />
-  </>
+  </React.StrictMode>
 );
