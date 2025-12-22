1bde411b9611b714856bc82965be20dade0c98a0
Sandeep <sandeep12pcm@gmail.com>
2025-12-22 18:43:02 +0530

    added PWA and offline landing page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -13,3 +13,9 @@ createRoot(document.getElementById("root")!).render(
     <Analytics />
   </React.StrictMode>
 );
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
